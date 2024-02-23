b50f8f8006cd8aa386af04f08f4605d9504fb897
Flo <flo@uwap.org>
2024-02-24 00:30:27 +0100

    Added Restore to backup.js.

--- Files/backup.js
+++ Files/backup.js
@@ -12,3 +12,24 @@ async function BackupNow(fresh) {
     warning.style["display"] = "none";
     ShowError("Connection failed.");
 }
+
+async function Restore(id) {
+    var btn = document.querySelector("#restore-" + id);
+    if (btn.innerText === "Restore...")
+        return;
+    if (btn.innerText !== "Restore?") {
+        btn.innerText = "Restore?";
+        return;
+    }
+    btn.innerText = "Restore...";
+    try {
+        let response = await fetch("/api[PATH_PREFIX]/backups/restore?id=" + id);
+        if (response.status === 200) {
+            window.location.reload();
+            return;
+        }
+    } catch {
+    }
+    btn.innerText = "Restore :(";
+    ShowError("Connection failed.");
+}
