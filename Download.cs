db9400d59876e97b9b485df360b31de4ec298dfd
Flo <flo@uwap.org>
2024-02-24 00:33:14 +0100

    Added a download handler.

---
+++ Download.cs
@@ -0,0 +1,19 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class ServerPlugin : Plugin
+{
+    public override async Task Handle(DownloadRequest req, string path, string pathPrefix)
+    {
+        if (!req.IsAdmin())
+        {
+            req.Status = 403;
+            return;
+        }
+        switch (path)
+        {
+            default:
+                req.Status = 404;
+                break;
+        }
+    }
+}
