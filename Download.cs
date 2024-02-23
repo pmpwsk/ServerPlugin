fcb5f3f97e7951e8dca2a949e0e6937ff1a1030b
Flo <flo@uwap.org>
2024-02-24 00:33:24 +0100

    Added /dl/backup.

--- Download.cs
+++ Download.cs
@@ -1,4 +1,6 @@
-﻿namespace uwap.WebFramework.Plugins;
+﻿using System.IO.Compression;
+
+namespace uwap.WebFramework.Plugins;
 
 public partial class ServerPlugin : Plugin
 {
@@ -11,6 +13,30 @@ public partial class ServerPlugin : Plugin
         }
         switch (path)
         {
+            case "/backup":
+                if (!AllowBackupManagement)
+                {
+                    req.Status = 403;
+                    break;
+                }
+                if ((!req.Query.TryGetValue("id", out var id)) || !long.TryParse(id, out _))
+                {
+                    req.Status = 400;
+                    break;
+                }
+                string dir = $"{Server.Config.Backup.Directory}{id}";
+                if (!Directory.Exists(dir))
+                {
+                    req.Status = 404;
+                    break;
+                }
+                string zip = $"{dir}.zip";
+                if (File.Exists(zip))
+                    File.Delete(zip);
+                ZipFile.CreateFromDirectory(dir, zip, CompressionLevel.Optimal, false);
+                await req.SendFile(zip, $"backup-{id}.zip");
+                File.Delete(zip);
+                break;
             default:
                 req.Status = 404;
                 break;
