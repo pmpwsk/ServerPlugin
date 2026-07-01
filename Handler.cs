5313d0b07ee9f5982b18729696c74b2a42391d6d
pmpwsk <flo@uwap.org>
2026-07-01 22:41:03 +0200

    Reworked plugin endpoints with method attributes.

--- Handler.cs
+++ Handler.cs
@@ -4,7 +4,7 @@ namespace uwap.WebFramework.Plugins;
 
 public partial class ServerPlugin : Plugin
 {
-    public override Task<IResponse> HandleAsync(Request req)
+    public override Task<IResponse> HandleOtherAsync(Request req)
         => Parsers.GetFirstSegment(req.Path, out _) switch
         {
             "wrapper" => HandleWrapper(req),
